579c0376ebd6751dc07e411387f56c15bbcd73aa
虚空 <1542989873@qq.com>
2021-09-16 10:29:57 +0800

    格式化代码

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,37 +8,37 @@
 </template>
 
 <script>
-  import NavBar from 'components//common/navbar/NavBar'
-  import {getHomeMultiData} from 'network/home'
-  import Swiper from 'components/swiper/Swiper'
+import NavBar from 'components//common/navbar/NavBar'
+import {getHomeMultiData} from 'network/home'
+import Swiper from 'components/swiper/Swiper'
 
-  export default {
-    name: "Home",
-    components: {
-      NavBar,
-      Swiper
-    },
-    data() {
-      return {
-        banners: [],
-        recommends: [],
-        dKeyword: [],
-        keywords: []
-      }
-    },
-    created() {
-      getHomeMultiData().then(res => {
-        console.log(res)
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    Swiper
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+      dKeyword: [],
+      keywords: []
     }
+  },
+  created() {
+    getHomeMultiData().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
   }
+}
 </script>
 
 <style scoped>
-  .home-nav {
-    background-color: var(--color-tint);
-    color: white;
-  }
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+}
 </style>
